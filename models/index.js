1cb62cc3f071b55bddd1039f0c8f4bf1a1dad09c
devin-lynch <devin.l.lynch@gmail.com>
2022-10-04 11:51:20 -0500

    Add drink model

---
+++ models/index.js
@@ -0,0 +1,17 @@
+// require the mongoose package
+const mongoose = require('mongoose')
+
+// tell mongoose what our database uri is and connect to it
+const uri = 'mongodb://127.0.0.1/mongooseIntro'
+mongoose.connect(uri)
+
+// use mongoose's connections methods to validate the database collection (useful console.logs)
+const db = mongoose.connection
+
+// connection success
+db.once('open', () => console.log(`mongoDB connected @ ${db.host}:${db.port} ⛓`))
+// connection failure
+db.on('error', err => console.warn('🔥 the data center has burned down', err))
+
+// export all of our mongoose models from this file
+
